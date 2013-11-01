82319cb3b5b27aa887a55dd3f1e3efc3fb8fbc00
Manuel Mujica <mmujicarojas@groupon.com>
2013-11-01 20:01:10 -0300

    Add server side logic for SocketIO.

--- chatrooms/server.js
+++ chatrooms/server.js
@@ -55,3 +55,7 @@ var server = http.createServer(function (request, response) {
 server.listen(3000, function() {
   console.log('Server listening on port 30000');
 });
+
+var chatServer = require('./lib/chat_server');
+chatServer.listen(server);
+
